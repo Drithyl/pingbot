2adf552ec834353a61aa81dd525a5440b869ae9e
Drithyl <hugo.bujan@gmail.com>
2024-01-01 14:39:20 -0500

    Restructured events to follow same dir structure as commands

--- events/events-index.js
+++ events/events-index.js
@@ -3,23 +3,33 @@ module.exports = loadEvents;
 function loadEvents(client) {
 	const fs = require('node:fs');
 	const path = require('node:path');
-	const eventsPath = path.join(__dirname);
-	const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-	for (const file of eventFiles) {
-		const filePath = path.join(eventsPath, file);
-		const event = require(filePath);
+	const foldersPath = path.join(__dirname);
 
-		// Probably events-index.js, so ignore
-		if (event.execute === undefined) {
-			continue;
-		}
+	// Get an array of directory names within the events directory, filtering out all non-folder results (i.e. events-index.js)
+	const eventFolders = fs.readdirSync(foldersPath, { withFileTypes: true })
+		.filter(dirent => dirent.isDirectory())
+		.map(dirent => dirent.name);
 
-		if (event.once !== undefined) {
-			client.once(event.name, (...args) => event.execute(...args));
-		}
-		else {
-			client.on(event.name, (...args) => event.execute(...args));
+	for (const folder of eventFolders) {
+		const eventsPath = path.join(foldersPath, folder);
+		const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+		for (const file of eventFiles) {
+			const filePath = path.join(eventsPath, file);
+			const event = require(filePath);
+
+			// Probably events-index.js, so ignore
+			if (event.execute === undefined) {
+				continue;
+			}
+
+			if (event.once !== undefined) {
+				client.once(event.name, (...args) => event.execute(...args));
+			}
+			else {
+				client.on(event.name, (...args) => event.execute(...args));
+			}
 		}
 	}
 }
